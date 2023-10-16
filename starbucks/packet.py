e3102ffe4f07cd3d1e452d04c71e7befde00e85f
damianu <damian@datagentleman.io>
2023-10-16 20:10:04 +0200

    decode message from bytes

--- starbucks/packet.py
+++ starbucks/packet.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import socket
 
 # Low level reading/writing packets from/to tcp sockets
@@ -29,19 +30,34 @@ class Message:
   def to_bytes(self) -> bytearray:
     cmd = write_packet(self.cmd.encode())
     
-    args = []
-    for key in self.args:
-      args.append(write_packet(key.encode()))
-    
+    args = [write_packet(key.encode()) for key in self.args]
     args_count = write_packet(len(args).to_bytes(2))
-    msg = cmd + args_count + b''.join(args)
-    return msg
+
+    return bytearray(cmd + args_count + b''.join(args))
+
+
+  @staticmethod
+  def from_bytes(data: bytearray|bytes) -> Message:
+    cmd = read_packet(data).decode()
     
+    args_count = int.from_bytes(read_packet(data), "big")
+    args = [read_packet(data).decode() for _ in range(args_count)]
+
+    return Message(cmd, *args)
+   
     
-def write_packet(data: bytearray|bytes) -> bytearray:
+def write_packet(data: bytearray|bytes) -> bytes|bytearray:
   size = len(data).to_bytes(2)
   return size + data
 
+
+def read_packet(data: bytearray) -> bytes|bytearray:  
+  bytes = data[:2]; del data[:2]
+  
+  size = int.from_bytes(bytes, byteorder='big')
+  packet = data[:size]; del data[:size]
+
+  return packet
       
       
       
--- starbucks/tests/test_packet.py
+++ starbucks/tests/test_packet.py
@@ -1,5 +1,8 @@
 from starbucks.packet import Message
 
 def test_packet_message_to_bytes():
-  msg = Message("Hello", "World")
-  print(msg.to_bytes())
+  bytes = Message("Hello", "World").to_bytes()
+  msg = Message.from_bytes(bytes)
+
+  assert(msg.cmd == "Hello")
+  assert(msg.args == ("World",))
