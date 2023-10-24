bb3fd039de30f792fb4f82c3ee895210f44ac380
damianu <damian@datagentleman.io>
2023-10-24 21:31:49 +0200

    remove tensors afters testing

--- starbucks/tests/tensor_test.py
+++ starbucks/tests/tensor_test.py
@@ -2,7 +2,7 @@ import os
 from starbucks.tensor import Tensor
 
 def test_tensor_create_remove():
-  path = 'iris/validation/w1'
+  path = 'test/iris/validation/w1'
 
   Tensor.create(path)
   assert(os.path.exists(Tensor.ROOT + path))
@@ -12,10 +12,12 @@ def test_tensor_create_remove():
   
 
 def test_tensor_ls():
-  Tensor.remove("iris")
-  Tensor.create("iris/validation/w1")
+  Tensor.create("test/iris/validation/w1")
   
   tensors = Tensor.ls()
   
   assert(len(tensors) == 1)
-  assert(tensors[0] == ('iris', 'validation', 'w1'))
+  assert(tensors[0] == ('test', 'iris', 'validation', 'w1'))
+  
+  Tensor.remove("test")
+  
