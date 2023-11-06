700035e7ed540b51fb04182414876025e59f4d2d
damianu <damian@datagentleman.io>
2023-11-06 17:16:36 +0100

    display bytes(char*) in cpp

--- starbucks/src/cpp/utils.cpp
+++ starbucks/src/cpp/utils.cpp
@@ -1,7 +1,27 @@
-// taking 2 bytes from bytes and converting them to uint16.
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// reading 2 bytes and converting them to uint16
 uint16_t to_uint16(char* bytes, bool big_endian=true) {
   if (big_endian) {
     return (bytes[0] << 8) | bytes[1];
+  }
+}
+
+// display bytes (char*)
+void display(char* data, int size, bool as_bytes=false) {
+  if(as_bytes) {
+    for(int i=0; i < size; i++) {
+      std::cout << std::hex << "\\x" << static_cast<int>(data[i]);
+    }
+    return;
+  } 
+
+  for(int i=0; i < size; i++) {
+    std::cout << data[i];
   }
 }
 
