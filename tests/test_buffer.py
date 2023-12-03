f786b038bcbed9364904b213f834494ad03ebd08
damianu <damian@datagentleman.io>
2023-12-03 22:52:29 +0100

    uncomment working tests

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -7,11 +7,11 @@ def test_buffer_write_read():
   buf.write(1)
   assert(buf.read('int') == 1)
 
-  # buf.write(1.0001)
-  # assert(round(buf.read('float'), 4) == 1.0001)
+  buf.write(1.0001)
+  assert(round(buf.read('float'), 4) == 1.0001)
 
-  # buf.write("bubblegum")
-  # assert(buf.read('str') == "bubblegum")
+  buf.write("bubblegum")
+  assert(buf.read('str') == "bubblegum")
   
-  # buf.write((1, 2, 3))
-  # assert(buf.read('list', 'int') == [1, 2, 3])
+  buf.write((1, 2, 3))
+  assert(buf.read('list', 'int') == [1, 2, 3])
