6dbf44f4aa758f1432c9539c48146a5516c040c2
damianu <damian@datagentleman.io>
2023-12-11 11:50:44 +0100

    refactor timer

--- lib/cpp/timer.cpp
+++ lib/cpp/timer.cpp
@@ -1,21 +1,23 @@
 #include <chrono>
 
+using namespace std::chrono;
+
 class Timer {
   public:
     Timer(const char* name) : name(name) {
-      start = std::chrono::high_resolution_clock::now();
+      start = high_resolution_clock::now();
     }
 
     ~Timer() {
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+      auto end = high_resolution_clock::now();
+      auto seconds = duration_cast<duration<double>>(end - start);
 
       std::cout << name << " Execution Time: " << seconds.count() << " seconds" << std::endl;
     }
 
   private:
     const char* name;
-    std::chrono::high_resolution_clock::time_point start;
+    high_resolution_clock::time_point start;
 };
 
 #define TIMER_BEGIN(name) \
